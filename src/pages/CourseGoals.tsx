c635ecb43a67f65f15c5d50d0bf0f90068b90dd5
AlexandrMel <melnic.alexandr88@gmail.com>
2020-04-27 19:59:39 +0200

    added Course Goals and made them work dynamic, added necessary buttons

--- src/pages/CourseGoals.tsx
+++ src/pages/CourseGoals.tsx
@@ -1,22 +1,69 @@
 import React from 'react';
-import { IonHeader, IonContent, IonToolbar, IonTitle, IonPage, IonButtons, IonBackButton } from '@ionic/react';
+import { IonHeader, IonContent, IonToolbar, IonTitle, IonPage, IonButtons, IonBackButton, IonList, IonItem, IonLabel, IonButton, IonIcon, IonItemSliding, IonItemOptions, IonItemOption, IonFab, IonFabButton, isPlatform } from '@ionic/react';
 import { useParams } from 'react-router-dom'
 import { COURSE_DATA } from './Courses'
+import { golfSharp, trash, addOutline } from 'ionicons/icons';
+import { create } from 'ionicons/icons';
 const CourseGoals: React.FC = () => {
 const selectedCourseId = useParams<{courseId: "string"}>().courseId;
 const selectedCourse = COURSE_DATA.find(c => c.id === selectedCourseId);
+
+const deleteItemHandler = () => {
+    console.log('Deleted...')
+}
+const startEditGoalHandler = (event: React.MouseEvent) => {
+    event.stopPropagation()
+    console.log('Editing...')
+}
+const startAddGoalHandler = (event: React.MouseEvent) => {
+    console.log('Adding goal...')
+}
+
     return (
         <IonPage>
         <IonHeader>
             <IonToolbar>
                 <IonButtons slot="start">
-                    <IonBackButton defaultHref="/" />
+                    <IonBackButton defaultHref="/courses.list" />
                 </IonButtons>
                 <IonTitle>{selectedCourse ? selectedCourse.title : 'No course found!'}</IonTitle>
+{!isPlatform('android') && (
+                    <IonButtons slot="end">
+                    <IonButton onClick={startAddGoalHandler}>
+                        <IonIcon slot="icon-only" icon={addOutline}/>
+                    </IonButton>
+                </IonButtons>
+)}
             </IonToolbar>
         </IonHeader>
         <IonContent>
-            <h2>This works - course goals page!!!</h2>
+            {selectedCourse && <IonList>
+                {selectedCourse?.goals.map(goal => (
+                    <IonItemSliding key={goal.id} >
+                        <IonItemOptions side="start">
+                            <IonItemOption onClick={deleteItemHandler} color="danger">
+                                <IonIcon slot="icon-only" icon={trash} />
+                            </IonItemOption>
+                        </IonItemOptions>
+                    <IonItem button onClick={deleteItemHandler}>
+                        <IonLabel>{goal.text}</IonLabel>
+                    </IonItem>
+                    <IonItemOptions side="end">
+                            <IonItemOption onClick={startEditGoalHandler}>
+                                <IonIcon slot="icon-only" icon={create} />
+                            </IonItemOption>
+                        </IonItemOptions>
+                    </IonItemSliding>
+                )
+                )}
+                </IonList>}
+                {isPlatform('android') && (
+                    <IonFab horizontal="end" vertical="bottom" slot="fixed">
+                    <IonFabButton color="secondary" onClick={startAddGoalHandler}>
+                        <IonIcon icon={addOutline} />
+                    </IonFabButton>
+                </IonFab>
+                )}
         </IonContent>
         </IonPage>
     )
--- src/pages/Courses.tsx
+++ src/pages/Courses.tsx
@@ -11,6 +11,9 @@ import {
   IonCol,
   IonCard,
   IonCardContent,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardSubtitle,
 } from "@ionic/react";
 // import { useHistory } from 'react-router-dom'
 
@@ -18,14 +21,44 @@ export const COURSE_DATA = [
   {
     id: "c1",
     title: "Ionic + React - The Practical Guide",
+    enrolled: new Date("03/22/2019"),
+    goals: [
+        {id: 'c1g1', text: 'Finish the course!'},
+        {id: 'c1g1', text: 'Learn a lot!'}
+    ]
   },
   {
     id: "c2",
     title: "React.js - The Complete Guide",
+    enrolled: new Date("05/15/2018"),
+    goals: [
+        {id: 'c2g1', text: 'Finish the course!'},
+        {id: 'c2g1', text: 'Learn a lot!'}
+    ]
   },
   {
     id: "c3",
     title: "Javascript - The Complete Guide",
+    enrolled: new Date("01/22/2020"),
+    goals: [
+        {id: 'c3g1', text: 'Finish the course!'},
+        {id: 'c3g2', text: 'Learn a lot!'},
+        {id: 'c3g3', text: 'Learn a lot!'},
+        {id: 'c3g4', text: 'Learn a lot!'},
+        {id: 'c3g5', text: 'Learn a lot!'},
+        {id: 'c3g6', text: 'Learn a lot!'},
+        {id: 'c3g7', text: 'Learn a lot!'},
+        {id: 'c3g8', text: 'Learn a lot!'},
+        {id: 'c3g9', text: 'Learn a lot!'},
+        {id: 'c3g10', text: 'Learn a lot!'},
+        {id: 'c3g11', text: 'Learn a lot!'},
+        {id: 'c3g12', text: 'Learn a lot!'},
+        {id: 'c3g13', text: 'Learn a lot!'},
+        {id: 'c3g14', text: 'Learn a lot!'},
+        {id: 'c3g15', text: 'Learn a lot!'},
+        {id: 'c3g16', text: 'Learn a lot!'},
+        {id: 'c3g17', text: 'Learn a lot!'},
+    ]
   },
 ];
 const Courses: React.FC = () => {
@@ -41,21 +74,42 @@ const Courses: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-       <IonGrid>
-           {COURSE_DATA.map(course => (
-           <IonRow key={course.id}>
-           <IonCol className="ion-text-center" size-md="4" offset-md="4">
-               <IonCard>
-                   <IonCardContent>
-           <h2 >{course.title}</h2>
-           <IonButton routerLink={`/courses/${course.id}`}>View Course Goals</IonButton>
-                   </IonCardContent>
-               </IonCard>
-           </IonCol>
-       </IonRow>
-           ))}
-
-       </IonGrid>
+        <IonGrid>
+          {COURSE_DATA.map((course) => (
+            <IonRow key={course.id}>
+              <IonCol size-md="4" offset-md="4">
+                <IonCard>
+                  <IonCardHeader>
+                    <IonCardTitle>
+                      <h2>{course.title}</h2>
+                    </IonCardTitle>
+                    <IonCardSubtitle>
+                      <h3>
+                        Enrolled on{" "}
+                        {course.enrolled.toLocaleDateString("en-US", {
+                          year: "numeric",
+                          month: "2-digit",
+                          day: "2-digit",
+                        })}
+                      </h3>
+                    </IonCardSubtitle>
+                  </IonCardHeader>
+                  <IonCardContent>
+                    <div className="ion-text-right">
+                      <IonButton
+                        fill="clear"
+                        color="secondary"
+                        routerLink={`/courses/${course.id}`}
+                      >
+                        View Course Goals
+                      </IonButton>
+                    </div>
+                  </IonCardContent>
+                </IonCard>
+              </IonCol>
+            </IonRow>
+          ))}
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
